3fff887a47e7708dad30125a7cc78ecc512a44b7
Ismail Kamran <hexhalifax@hexs-MacBook-Air.local>
2025-06-26 14:48:38 +0100

    Add variable declarations for secrets

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,18 @@ variable "aws_region" {
   description = "AWS region"
   default     = "eu-west-2"
 }
+
+variable "FLASK_SECRET" {
+  type      = string
+  sensitive = true
+}
+
+variable "OIDC_CLIENT_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "OIDC_CLIENT_SECRET" {
+  type      = string
+  sensitive = true
+}
